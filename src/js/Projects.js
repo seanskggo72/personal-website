718756fb55345bcee0ff6ab20791d04d2856f5dd
Sean <seanskg72@gmail.com>
2021-01-27 22:47:56 +1100

    :tada: transition and lazyloading added

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -8,6 +8,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 import 'intersection-observer';
+import handleViewport from 'react-in-viewport';
 import React from "react";
 import '../css/Projects.css';
 
@@ -19,11 +20,56 @@ import '../css/Projects.css';
 // Functions
 /////////////////////////////////////////////////////////////////////////////////
 
+const animation = (props) => {
+  const { inViewport, enterCount } = props;
+  //Fade in only the first time we enter the viewport
+  if (inViewport && enterCount === 1) {
+    return { WebkitTransition: 'opacity 0.75s ease-in-out' };
+  } else if (!inViewport && enterCount < 1) {
+    return { WebkitTransition: 'none', opacity: '0' };
+  } else {
+    return {};
+  }
+}
+
+const Block = (props) => {
+  const { forwardedRef } = props;
+  return (
+    <div ref={forwardedRef} style={animation(props)}>
+      <h3>Hi YEET!!!</h3>
+      <div style={{ width: '400px', height: '300px', background: 'blue' }} />
+    </div>
+  );
+};
+
+const Block2 = (props) => {
+  const { forwardedRef } = props;
+  return (
+    <div ref={forwardedRef} style={animation(props)}>
+      <h3>Hi YEET!!!</h3>
+      <div style={{ width: '400px', height: '300px', background: 'red' }} />
+    </div>
+  );
+};
+
+const ViewportBlock = handleViewport(Block);
+const ViewportBlock2 = handleViewport(Block2);
+
 const Project_template = () => {
   return (
     <div className='Main'>
-      <text>Projects coming soon...</text>
+      <div>
+        <text>YEET!!</text>
+      </div>
+      <ViewportBlock />
+      <ViewportBlock />
+      <ViewportBlock />
+      <ViewportBlock2 />
     </div>
+
+    // <div className='Main'>
+    //   <text>Projects coming soon...</text>
+    // </div>
   );
 }
 
