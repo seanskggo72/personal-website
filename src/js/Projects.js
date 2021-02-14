7bd0d7c4f48b4ac4591c0dcded43762a60fabc10
Sean <seanskg72@gmail.com>
2021-02-14 19:41:08 +1100

    additional project added

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -14,6 +14,7 @@ import '../css/App.css';
 import TicTacToe from '../assets/TicTacToe.png';
 import Vscode from '../assets/vscode.png';
 import Bule from '../assets/bule.png';
+import Churn from '../assets/churn.png';
 
 /////////////////////////////////////////////////////////////////////////////////
 // Functions
@@ -42,8 +43,8 @@ const Slot_1 = (props) => {
           <div className='Right_sub_slot Column'>
             <text className='Title Sub_Header'>Tic Tac Toe AI</text>
             <text className='Text Header Component_text Paragraph_container'>
-              Simple Tic Tac Toe app created with React Native and Expo from scratch using
-              functional components and hooks.
+              Mobile app created with React Native and Expo from scratch using
+              functional components, hooks and Minimax algorithm.
             </text>
             <div>
               <button className='Button Button_styling'
@@ -73,7 +74,7 @@ const Slot_2 = (props) => {
       <div className='FlexBox Main_slot'>
         <div className='FlexBox Sub_slot'>
           <div className='Left_sub_slot'>
-            <img src={Vscode} className='Image2' alt='Tic Tac Toe App' />
+            <img src={Vscode} className='Image2' alt='Bule Vscode Extension' />
           </div>
           <div className='Right_sub_slot Column'>
             <text className='Title Sub_Header'>Bule Syntax Highlighter</text>
@@ -120,13 +121,46 @@ const Slot_3 = (props) => {
       <div className='FlexBox Main_slot'>
         <div className='FlexBox Sub_slot'>
           <div className='Left_sub_slot'>
-            <img src={Bule} className='Image3' alt='Tic Tac Toe App' />
+            <img src={Bule} className='Image3' alt='Reserach contribution' />
           </div>
           <div className='Right_sub_slot Column'>
             <text className='Title Sub_Header'>Efficacy of Bule as a SAT language</text>
             <text className='Text Header Component_text Paragraph_container'>
               Ongoing research on Bule and its efficiency of representing
               boolean expressions, factoring in time complexity and compactness.
+            </text>
+            <div>
+              <button className='Button Button_styling'
+                onClick={() => Redirector('https://github.com/AbdallahS/pspace-encodings')}>
+                GITHUB
+              </button>
+              <button className='Button Button_styling'
+                onClick={() => Redirector('https://github.com/vale1410/bule')}>
+                BULE
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Slot_4 = (props) => {
+  const { forwardedRef } = props;
+  return (
+    <div ref={forwardedRef} className={Animation(props, 'Header')}>
+      <div className='FlexBox Main_slot'>
+        <div className='FlexBox Sub_slot'>
+          <div className='Left_sub_slot'>
+            <img src={Churn} className='Image4' alt='Churn Predictor Website' />
+          </div>
+          <div className='Right_sub_slot Column'>
+            <text className='Title Sub_Header'>Customer Churn Predictor</text>
+            <text className='Text Header Component_text Paragraph_container'>
+              Predicts likelihood of bank attrition. Uses multiple regression
+              as machine learning model. Created with React, Flask, Heroku and Firebase. 
+              REST-based application.
             </text>
             <div>
               <button className='Button Button_styling'
@@ -154,6 +188,7 @@ const Slot1 = handleViewport(Slot_1);
 const Slot2 = handleViewport(Slot_2);
 const Banner = handleViewport(Banner_1);
 const Slot3 = handleViewport(Slot_3);
+const Slot4 = handleViewport(Slot_4);
 
 /////////////////////////////////////////////////////////////////////////////////
 // Exports
@@ -170,8 +205,9 @@ const Project_template = () => {
           technologies used.</text>
         </div>
       </div>
-      <Slot1 />
       <Slot2 />
+      <Slot4 />
+      <Slot1 />
       <Banner />
       <Slot3 />
     </div>
