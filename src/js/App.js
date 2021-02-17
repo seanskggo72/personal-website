5a3a81cf732cba014a8a3ec239facdddd7cd06f6
Sean <seanskg72@gmail.com>
2021-02-17 13:56:16 +1100

    compressed background

--- src/js/App.js
+++ src/js/App.js
@@ -15,7 +15,7 @@ import {
   Link
 } from "react-router-dom";
 import sean from '../svg/sean.png';
-import background from '../svg/wallpaper.svg'
+import background from '../svg/wallpaper.png'
 import Typewriter from 'typewriter-effect';
 import Resume from './Resume';
 import Projects from './Projects';
@@ -59,7 +59,7 @@ const Home = () => {
         <Typewriter
           className='Typewriter'
           options={{ cursor: '_', delay: 65 }}
-          onInit={(typewriter) => { typewriter.typeString(bio).start() }}
+          onInit={(typewriter) => { typewriter.pauseFor(2000).typeString(bio).start() }}
         />
         <div className='Div_margin'>
           <Link to="/portfolio" tabIndex="-1" className='Main_button'>
